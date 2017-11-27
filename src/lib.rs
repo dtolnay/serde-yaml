bfb3522984f98fad75101a562eeed9e21da322f7
David Tolnay <dtolnay@gmail.com>
2017-11-27 08:31:17 -0800

    Release 0.7.3

--- src/lib.rs
+++ src/lib.rs
@@ -60,7 +60,7 @@
 //! # }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/serde_yaml/0.7.2")]
+#![doc(html_root_url = "https://docs.rs/serde_yaml/0.7.3")]
 
 #![cfg_attr(feature = "cargo-clippy", deny(clippy, clippy_pedantic))]
 // Whitelisted clippy_pedantic lints
