87f0dca0eb500c6446e552dd263412da1a0e1fd1
David Tolnay <dtolnay@gmail.com>
2018-06-10 18:27:17 -0700

    Release 0.7.5

--- src/lib.rs
+++ src/lib.rs
@@ -60,7 +60,7 @@
 //! # }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/serde_yaml/0.7.4")]
+#![doc(html_root_url = "https://docs.rs/serde_yaml/0.7.5")]
 #![cfg_attr(feature = "cargo-clippy", deny(clippy, clippy_pedantic))]
 // Whitelisted clippy lints
 #![cfg_attr(feature = "cargo-clippy", allow(redundant_field_names))]
