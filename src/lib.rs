f9509c81c5596f0f2592b8e0c4cdc09c33df3c23
David Tolnay <dtolnay@gmail.com>
2019-05-02 14:07:12 -0700

    Release 0.8.9

--- src/lib.rs
+++ src/lib.rs
@@ -57,7 +57,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/serde_yaml/0.8.8")]
+#![doc(html_root_url = "https://docs.rs/serde_yaml/0.8.9")]
 #![deny(missing_docs)]
 #![cfg_attr(feature = "cargo-clippy", allow(renamed_and_removed_lints))]
 #![cfg_attr(feature = "cargo-clippy", deny(clippy, clippy_pedantic))]
