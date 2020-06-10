c3c5758b546069bb7c3c35337772ed0ade5b0531
David Tolnay <dtolnay@gmail.com>
2020-06-10 00:05:23 -0700

    Release 0.8.13

--- src/lib.rs
+++ src/lib.rs
@@ -65,7 +65,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/serde_yaml/0.8.12")]
+#![doc(html_root_url = "https://docs.rs/serde_yaml/0.8.13")]
 #![deny(missing_docs)]
 #![deny(clippy::all, clippy::pedantic)]
 // Suppressed clippy_pedantic lints
