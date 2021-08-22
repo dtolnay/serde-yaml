16b7f2f1d29bbc30b4f887cf60ddd7c6d88bec0c
David Tolnay <dtolnay@gmail.com>
2021-08-21 17:21:59 -0700

    Provide a handwritten PartialOrd impl for Mapping

--- src/mapping.rs
+++ src/mapping.rs
@@ -3,6 +3,7 @@
 use crate::Value;
 use indexmap::IndexMap;
 use serde::{Deserialize, Deserializer, Serialize};
+use std::cmp::Ordering;
 use std::collections::hash_map::DefaultHasher;
 use std::fmt;
 use std::hash::{Hash, Hasher};
@@ -136,6 +137,85 @@ impl Hash for Mapping {
             xor ^= hasher.finish();
         }
         xor.hash(state);
+    }
+}
+
+impl PartialOrd for Mapping {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let mut self_entries = Vec::from_iter(self);
+        let mut other_entries = Vec::from_iter(other);
+
+        // Sort in an arbitrary order that is consistent with Value's PartialOrd
+        // impl.
+        fn total_cmp(a: &Value, b: &Value) -> Ordering {
+            match (a, b) {
+                (Value::Null, Value::Null) => Ordering::Equal,
+                (Value::Null, _) => Ordering::Less,
+                (_, Value::Null) => Ordering::Greater,
+
+                (Value::Bool(a), Value::Bool(b)) => a.cmp(b),
+                (Value::Bool(_), _) => Ordering::Less,
+                (_, Value::Bool(_)) => Ordering::Greater,
+
+                (Value::Number(a), Value::Number(b)) => a.total_cmp(b),
+                (Value::Number(_), _) => Ordering::Less,
+                (_, Value::Number(_)) => Ordering::Greater,
+
+                (Value::String(a), Value::String(b)) => a.cmp(b),
+                (Value::String(_), _) => Ordering::Less,
+                (_, Value::String(_)) => Ordering::Greater,
+
+                (Value::Sequence(a), Value::Sequence(b)) => iter_cmp_by(a, b, total_cmp),
+                (Value::Sequence(_), _) => Ordering::Less,
+                (_, Value::Sequence(_)) => Ordering::Greater,
+
+                (Value::Mapping(a), Value::Mapping(b)) => {
+                    iter_cmp_by(a, b, |(ak, av), (bk, bv)| {
+                        total_cmp(ak, bk).then_with(|| total_cmp(av, bv))
+                    })
+                }
+            }
+        }
+
+        fn iter_cmp_by<I, F>(this: I, other: I, mut cmp: F) -> Ordering
+        where
+            I: IntoIterator,
+            F: FnMut(I::Item, I::Item) -> Ordering,
+        {
+            let mut this = this.into_iter();
+            let mut other = other.into_iter();
+
+            loop {
+                let x = match this.next() {
+                    None => {
+                        if other.next().is_none() {
+                            return Ordering::Equal;
+                        } else {
+                            return Ordering::Less;
+                        }
+                    }
+                    Some(val) => val,
+                };
+
+                let y = match other.next() {
+                    None => return Ordering::Greater,
+                    Some(val) => val,
+                };
+
+                match cmp(x, y) {
+                    Ordering::Equal => {}
+                    non_eq => return non_eq,
+                }
+            }
+        }
+
+        // While sorting by map key, we get to assume that no two keys are
+        // equal, otherwise they wouldn't both be in the map. This is not a safe
+        // assumption outside of this situation.
+        let total_cmp = |&(a, _): &_, &(b, _): &_| total_cmp(a, b);
+        self_entries.sort_by(total_cmp);
+        other_entries.sort_by(total_cmp);
+        self_entries.partial_cmp(&other_entries)
     }
 }
 
