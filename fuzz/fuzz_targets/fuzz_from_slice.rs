7bd32c1799cf1964973f2a2f902b88709c77326d
David Tolnay <dtolnay@gmail.com>
2022-07-30 11:41:56 -0700

    Limit fuzzer input to 10K

--- fuzz/fuzz_targets/fuzz_from_slice.rs
+++ fuzz/fuzz_targets/fuzz_from_slice.rs
@@ -3,5 +3,6 @@
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
+    let data = data.get(..10240).unwrap_or(data);
     let _ = serde_yaml::from_slice::<serde_yaml::Value>(data);
 });
