b7a82ee9072c01b6abbae8012d2ab4356bfff1b5
David Tolnay <dtolnay@gmail.com>
2022-07-31 08:06:03 -0700

    Early return on too big fuzz case

--- fuzz/fuzz_targets/fuzz_from_slice.rs
+++ fuzz/fuzz_targets/fuzz_from_slice.rs
@@ -3,6 +3,7 @@
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    let data = data.get(..10240).unwrap_or(data);
-    let _ = serde_yaml::from_slice::<serde_yaml::Value>(data);
+    if data.len() <= 10240 {
+        let _ = serde_yaml::from_slice::<serde_yaml::Value>(data);
+    }
 });
