653e0bb9b34712d432c6cf405114e3f72fe51d99
David Tolnay <dtolnay@gmail.com>
2022-08-31 00:31:57 -0700

    Ignore using destructuring assignment to wildcard pattern

--- fuzz/fuzz_targets/fuzz_from_slice.rs
+++ fuzz/fuzz_targets/fuzz_from_slice.rs
@@ -4,6 +4,6 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     if data.len() <= 10240 {
-        let _ = serde_yaml::from_slice::<serde_yaml::Value>(data);
+        _ = serde_yaml::from_slice::<serde_yaml::Value>(data);
     }
 });
