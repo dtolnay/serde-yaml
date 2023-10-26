8032b5c6f3ed570b91ce573a36acea552d12e554
David Tolnay <dtolnay@gmail.com>
2023-10-25 20:27:04 -0700

    Release 0.9.26

--- src/lib.rs
+++ src/lib.rs
@@ -115,7 +115,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/serde_yaml/0.9.25")]
+#![doc(html_root_url = "https://docs.rs/serde_yaml/0.9.26")]
 #![deny(missing_docs)]
 // Suppressed clippy_pedantic lints
 #![allow(
